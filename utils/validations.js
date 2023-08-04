2240128f259637b969acc35ae5e44ff4aa31b856
AlmogOrkabi <117260151+AlmogOrkabi@users.noreply.github.com>
2023-08-04 20:58:13 +0300

    Update validations.js

--- utils/validations.js
+++ utils/validations.js
@@ -62,8 +62,8 @@ function isValidPhotosArray(photoUrls) {
 //     return (isValidUserName(name) && hebOReng.test(name))
 // }
 export function isValidName(name) {
-    //const hebOReng = /^[A-Za-z\u0590-\u05FF '-]+$/; //checks for letters in hebrew and english only.also checks for the chars ' and -
-    const hebOReng = /^[\u0590-\u05FFa-zA-Z '-]+$/;
+    const hebOReng = /^[A-Za-z\u0590-\u05FF '-]+$/; //checks for letters in hebrew and english only.also checks for the chars ' and -
+    //const hebOReng = /^[\u0590-\u05FFa-zA-Z '-]+$/;
 
     return (isValidUserName(name) && hebOReng.test(name))
 }
